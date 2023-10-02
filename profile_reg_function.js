e511c5d2b75de71b4e0ce4e2798089b348a927ac
jahratunnisa <141713515+jahratunnisa@users.noreply.github.com>
2023-10-02 13:47:00 +0700

    Add files via upload

---
+++ profile_reg_function.js
@@ -0,0 +1,7 @@
+
+function profile(namaDepan, namaBelakang, email) {
+    console.log("Nama : " + namaDepan + " "+ namaBelakang)
+    console.log("Email : "+ email)
+}
+
+profile("jahra","tunisa","jahra@gmail.com")
